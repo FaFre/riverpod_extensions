54343ec7f277552853ede081f2524e7857377498
Fabian Freund <github@fabi.online>
2021-12-30 00:55:14 +0100

    created package

---
+++ example/riverpod_extensions_example.dart
@@ -0,0 +1,6 @@
+import 'package:riverpod_extensions/riverpod_extensions.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/riverpod_extensions.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library riverpod_extensions;
+
+export 'src/riverpod_extensions_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
